a34b063d423a8ab1a56ddf41e6f51cecd9b3e4f9
Marlovy Renteria Cuero <cuerorenteriamarlovy33@gmail.com>
2024-10-07 10:09:35 -0500

    subiendoo ejercicios de javascrip externo

---
+++ script.js
@@ -0,0 +1,14 @@
+// Usar prompt() para pedir nombre y edad
+let nombre = prompt("¿Cuál es tu nombre?");
+let edad = prompt("¿Cuántos años tienes?");
+
+// Declarar la variable CIUDAD
+let ciudad = "Quito";
+
+// Mostrar las variables en la consola
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Ciudad:", ciudad);
+
+// Mostrar un mensaje de bienvenida con alert()
+alert(`Hola ${nombre}, tienes ${edad} años y vives en ${ciudad}`);
